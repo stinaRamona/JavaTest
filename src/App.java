eb1a1064d59f5226c430a30d90826aec335c68e1
Stina Persson <stpe1901@student.miun.se>
2025-04-12 12:49:57 +0200

    Testat runt lite för kul

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        //test variabler
+        String greeting = "Tjena mittbena!"; 
+
+        System.out.println("Greeting säger " + greeting);
+
+        int greetingLenght = greeting.length();
+
+        System.out.println("Greeting har " + greetingLenght + " tecken!");
+        
+        //test för metod addera
+        int number1 = 2;
+        int number2 = 3; 
+
+        int number3 = Utils.addera(number1, number2); 
+
+        System.out.println(number1 + "+" + number2 + "=" + number3); //lär finnas snyggare sätt att skriva på
     }
 }
---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+    
+    //testmetod - nu flyttad hit för struktur mellan filer. DRY. 
+    public static int addera(int a, int b) {
+
+        //lägger ihop a och b 
+        return a + b; 
+    }
+}
